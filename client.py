7b780cb4fe6f1ad4b68c3ef6bc6d8651157567a1
RainbowAsteroids <rainbowasteroids@protonmail.com>
2018-05-22 01:37:52 +0000

    Adds option to choose what subreddit you would like to view

--- client.py
+++ client.py
@@ -10,9 +10,14 @@ import pyfiglet  # Optional subreddit headers
 """OAuth credentials: Remove these before commiting to source control"""
 #id = "bl_3aEPx20KMOw"
 #secret = "tmVzWv5OXSuY3Laj66vfHA0UN8w"
-id = "Lzf9MWt6VKQbGg"
-secret = "UaEoRGae6DWmoBz7uUhp4CSeTxY"
-agent = "SimpleReddit (by /u/dylanhamer13)"
+id = "shkmVpv3teoLYw"
+secret = "x4un2-02-cUy_23TIvvHd7j1E28"
+agent = "SimpleReddit"
+
+def filter(text):
+    if "r/" in text:
+        return text.split("/")[1]
+    else:return text
 
 class simpleReddit:
     def __init__(self):
@@ -20,7 +25,7 @@ class simpleReddit:
         self.reddit = praw.Reddit(user_agent=agent,
                              client_secret=secret,
                              client_id=id)
-        self.subreddit = "dadjokes"
+        self.subreddit = filter(input("What subreddit would you like to browse?\n"))
         self.sort = "hot"
 
     def browse(self, showScores = True):
